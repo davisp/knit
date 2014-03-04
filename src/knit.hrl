65b2da59812c2c592d1cb6ceb7728c955fb1e9f6
Paul J. Davis <paul.joseph.davis@gmail.com>
2014-03-03 19:25:43 -0600

    Add macros to normalize exit codes

--- src/knit.hrl
+++ src/knit.hrl
@@ -1,3 +1,9 @@
 
+-define(BAD_CLI(Msg, Args), knit_util:abort(1, Msg, Args)).
+-define(BAD_CONFIG(Msg, Args), knit_util:abort(2, Msg, Args)).
+-define(IO_ERROR(Msg, Args), knit_util:abort(3, Msg, Args)).
+-define(NOT_SUPPORTED(Msg, Args), knit_util:abort(126, Msg, Args)).
+-define(ABORT(Msg, Args), knit_util:abort(127, Msg, Args)).
+
 -define(TRACE(Msg), knit_log:trace(Msg, ?FILE, ?LINE)).
 -define(TRACE(Msg, Args), knit_log:trace(Msg, Args, ?FILE, ?LINE)).
